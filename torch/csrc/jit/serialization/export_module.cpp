1f13cad1f74b050a82a3fea3f3539e36305466bd
jakeszwe <jakeszwe@devvm1677.vll0.facebook.com>
2021-05-03 14:51:35 -0700

    [PyTorch Mobile][Forward/backward compatibility] Number of arguments for operators
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/56845
    
    
    Handle forward/backward compatibility caused by added default arguments in mobile. As an example,
    
    In older version, operator aten::foo's schema is
    ```
    foo(Tensor a, Tensor b) -> Tensor
    ```
    In the new version, the schema is updated to
    ```
    foo(Tensor a, Tensor b, int groups=1) -> Tensor
    ```
    
    ## Model file
    Serialize the number of specified arguments to each operator into the bytecode operator table. Before the operator table contains operator name and overload name:
    ```
    ('operators', (('aten::foo', ''),))
    ```
    Now the number of specified arguments is added:
    ```
    # bytecode version 6
    ('operators', (('aten::foo', '', 2),))
    ```
    where "2" means the number of specified arguments.
    
    Since there's bytecode schema change, the bytecode version number is bumped. This PR is to be landed after #56002 , where the version number is bumped from 4 to 5. This PR bumps the version number from 5 to 6.
    
    ## Runtime and backward compatibility
    When the operator is found (either jit or c10), we have the OperatorHandle, where the operator schema can be accessed by
    ```
    op.value().schema().arguments()
    ```
    Adaptation is implemented to handle backward compatibility. For the example above, the new runtime holds the updated schema:
    ```
    foo(Tensor a, Tensor b, int groups=1) -> Tensor
    ```
    Whereas the model file carries
    ```
    (('aten::foo', ''), 2)
    ```
    We can implement a wrapper around the original function pointer to push the default argument to the stack.
    
    ## Deliver time and forward compatibility
    At model delivery time, two checks can be done:
    ### Operator check
    Two APIs to be provided:
    * Runtime: An API to get a runtime’s ops and their schemas (i.e. the # of args). D27920185(WIP)
    * Model: An API to get a model’s ops and their schema requirements (i.e. the # of args required).
    
    The APIs can be used to check
    * runtime.ops() is a superset of model.ops()
    * for each op in model.ops() validate their schemas are compatible with those in runtime.ops() -- i.e. the # args required in a model op are <= # args in the runtime op.
    
    Note that only root ops in the model needs to be checked here. For transient ops it's not necessary. For example, if a root op, "aten::root" calls "aten::foo", it's "aten::root"'s responsibility to adapt to "aten::foo"'s change, or "aten::root" itself needs to be updated too.
    ### Bytecode version backport (PR coming)
    When delivering a model with bytecode v6, if the runtime only works with bytecode v5 and lower, backport is needed.
    * The number of arguments is removed from the operator table
    * The bytecode version is changed from 6 to 5
    
    Note that this backport is a pure format change, it does not guarantee the backported model always runs in old runtime. The operator check mentioned before should be done first, before it’s back ported to v5.
    
    Differential Revision: [D27986544](https://our.internmc.facebook.com/intern/diff/D27986544/)
    ghstack-source-id: 127482052

--- torch/csrc/jit/serialization/export_module.cpp
+++ torch/csrc/jit/serialization/export_module.cpp
@@ -198,9 +198,16 @@ std::pair<IValue, c10::optional<IValue>> getFunctionTuple(
 
   // operators
   std::vector<IValue> operators;
+  auto op_to_specified_args = code.op_to_num_specified_args();
   operators.reserve(opnames.size());
   for (const auto& opname : opnames) {
-    operators.emplace_back(Tup({opname.name, opname.overload_name}));
+    auto unique_name = c10::toString(opname);
+    int num_args = -1;
+    auto it = op_to_specified_args.find(unique_name);
+    if (it != op_to_specified_args.end()) {
+      num_args = it->second;
+    }
+    operators.emplace_back(Tup({opname.name, opname.overload_name, num_args}));
   }
 
   // constants
@@ -642,8 +649,10 @@ void export_opnames(const script::Module& m, std::set<std::string>& opnames) {
     for (const auto& op : ops_list) {
       auto op_item = op.toTuple()->elements();
       TORCH_CHECK(
-          op_item.size() == 2,
-          "There should be two parts in an operator name.");
+          op_item.size() == 2 || op_item.size() == 3,
+          "There should be either two parts (name and overload name), ",
+          "or three parts (name, overload name and number of specified args) ",
+          "for an operator");
       auto opname = op_item[0].toString()->string();
       auto overload = op_item[1].toString()->string();
       // NOLINTNEXTLINE(performance-inefficient-string-concatenation)
