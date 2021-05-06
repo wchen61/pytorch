fb4025bd5595798a034f1a39f2343e2ecdd2480f
yanlizhao <yanlizhao@fb.com>
2021-05-06 13:33:20 -0700

    [WIP] enable static graph training in DDP
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/55248
    
    This PR provides enable static graph training when users call _set_static_graph(). This can help support more use cases in DDP without performance regression, also can potentially improve performance when there are unused parameters in the graph.
    1. first iteration records graph states like how many times a grad is calculated, whether the grad is used or not. then first iteration queues a delay_all_reduce call back to all reduce grads.
    2. Since autograd call back is associated with current target graph task, the delay_all_all call back should be associated with out-most backward graph task. A DDP sink layer is added in DDP forward loop so that we can queue the delay_all_reduce call back in the sink layer.
    3. after first iterations, DDP will use the saved graph states to determine whether a grad is used or not. whether a grad is ready for communication.
    4. rebuilt bucket is called in second iteration, after graph states are recorded in first iteration.
    5. if the graph states change, DDP will throw errors
    ghstack-source-id: 128349494
    
    Differential Revision: [D27539964](https://our.internmc.facebook.com/intern/diff/D27539964/)

--- torch/testing/_internal/distributed/pipe_with_ddp_test.py
+++ torch/testing/_internal/distributed/pipe_with_ddp_test.py
@@ -1,6 +1,5 @@
 import torch
 import torch.distributed as dist
-import unittest
 
 from torch import nn
 from torch.nn.parallel import DistributedDataParallel
@@ -39,7 +38,6 @@ class PipeWithDDPTest(RpcAgentTestFixture):
     @requires_nccl()
     @dist_init
     @skip_if_rocm
-    @unittest.skip("DDP doesn't work with checkpointing")
     def test_basic_nccl_ckpt_always(self):
         self._run_basic_test("nccl", "always")
 
@@ -47,7 +45,6 @@ class PipeWithDDPTest(RpcAgentTestFixture):
     @requires_nccl()
     @dist_init
     @skip_if_rocm
-    @unittest.skip("DDP doesn't work with checkpointing")
     def test_basic_nccl_ckpt_except_last(self):
         self._run_basic_test("nccl", "except_last")
 
@@ -69,7 +66,6 @@ class PipeWithDDPTest(RpcAgentTestFixture):
     @requires_gloo()
     @dist_init
     @skip_if_rocm
-    @unittest.skip("DDP doesn't work with checkpointing")
     def test_basic_gloo_ckpt_always(self):
         self._run_basic_test("gloo", "always")
 
@@ -77,7 +73,6 @@ class PipeWithDDPTest(RpcAgentTestFixture):
     @requires_gloo()
     @dist_init
     @skip_if_rocm
-    @unittest.skip("DDP doesn't work with checkpointing")
     def test_basic_gloo_ckpt_except_last(self):
         self._run_basic_test("gloo", "except_last")
 
