1fa609bfd47043f051cc4682ef871769fbbe5862
jakeszwe <jakeszwe@devvm1677.vll0.facebook.com>
2021-05-06 14:49:20 -0700

    [Pytorch Edge] Model Ops compatibility api
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/57501
    
    Add an api _get_model_ops_and_info to get root operators and versioning info of a model in both cxx and python, and the input can be from a file path or buffer.
    ghstack-source-id: 128358973
    
    Differential Revision: [D28162765](https://our.internmc.facebook.com/intern/diff/D28162765/)

--- test/mobile/test_bytecode.py
+++ test/mobile/test_bytecode.py
@@ -1,8 +1,8 @@
-from torch.jit.mobile import _get_model_bytecode_version
+from torch.jit.mobile import _get_model_bytecode_version, _get_model_ops_and_info
 from torch.testing._internal.common_utils import TestCase, run_tests
 from pathlib import Path
 
-pytorch_test_dri = Path(__file__).resolve().parents[1]
+pytorch_test_dir = Path(__file__).resolve().parents[1]
 
 # script_module_v4.ptl and script_module_v5.ptl source code
 # class TestModule(torch.nn.Module):
@@ -22,9 +22,14 @@ pytorch_test_dri = Path(__file__).resolve().parents[1]
 
 class testVariousModelVersions(TestCase):
     def test_get_model_bytecode_version(self):
-        script_module_v4 = pytorch_test_dri / "cpp" / "jit" / "script_module_v4.ptl"
+        script_module_v4 = pytorch_test_dir / "cpp" / "jit" / "script_module_v4.ptl"
         version_v4 = _get_model_bytecode_version(script_module_v4)
         assert(version_v4 == 4)
+
+    def test_get_model_ops_and_info(self):
+        script_module_v6 = pytorch_test_dir / "cpp" / "jit" / "script_module_v6.ptl"
+        ops_v6 = _get_model_ops_and_info(script_module_v6)
+        assert(ops_v6["aten::linear"].num_schema_args == 3)
 
 if __name__ == '__main__':
     run_tests()
